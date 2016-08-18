99cb621e1c82066eb371830f6109e34515430677
Jordan Harband <ljharb@gmail.com>
2016-08-17 23:09:15 -0700

    Initial implementation.

---
+++ document-contains.js
@@ -0,0 +1,23 @@
+'use strict';
+/* globals document, Element */
+
+if (typeof document !== 'undefined' && !document.contains) {
+  document.contains = Element.prototype.contains = function contains(node) {
+    /* eslint prefer-rest-params: 0, no-param-reassign: 0 */
+    if (arguments.length < 1) {
+      throw new TypeError('1 argument is required');
+    }
+
+    do {
+      if (this === node) {
+        return true;
+      }
+      if (node) {
+        node = node.parentNode;
+      }
+    } while (node);
+
+    return false;
+  };
+}
+
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('airbnb-js-shims');
+
+// Fixes super-constructor calls in IE9/10
+require('ima-babel6-polyfill');
+
+// document.contains polyfill
+require('./document-contains');
+
+require('webrtc-adapter');
