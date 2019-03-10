a59abeb14920f72a6d4d6c527d574016a106d63a
Jordan Harband <ljharb@gmail.com>
2019-03-10 15:25:44 -0700

    [patch] extract `document.contains` shim to separate package

--- document-contains.js
+++ document-contains.js
@@ -1,27 +1,3 @@
 'use strict';
 
-/* globals document, Element */
-
-var shim;
-
-if (typeof document !== 'undefined' && !document.contains) {
-  shim = function contains(node) {
-    /* eslint prefer-rest-params: 0, no-param-reassign: 0 */
-    if (arguments.length < 1) {
-      throw new TypeError('1 argument is required');
-    }
-
-    do {
-      if (this === node) {
-        return true;
-      }
-      if (node) {
-        node = node.parentNode;
-      }
-    } while (node);
-
-    return false;
-  };
-  document.contains = shim;
-  Element.prototype.contains = shim;
-}
+require('document.contains/auto');
